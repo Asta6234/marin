33f9dd662837ed094fe595c43795015e3d2a7faa
One Eyed King <74711129+Asta6234@users.noreply.github.com>
2025-07-01 11:43:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://arno:MRpH8ZZhu4At4vXw@cluster0.8seupfa.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "reel2earn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "5738bada6e1fe0a93f87d737c031ca70dad4a9e9")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shortner.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "63c51dd5c5782435ced6cfb09c164e170ce2a9bf")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86000)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","Only_Fam/13")
