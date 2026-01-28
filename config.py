035b5dee6c95a468369aaca902933810bff4470d
One Eyed King <74711129+Asta6234@users.noreply.github.com>
2026-01-28 13:21:53 +0530

    Fix admin ID in config.py
    Removed an invalid admin ID from the ADMINS list.

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1392566136 2135561527)
+ADMINS.append(2135561527)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
